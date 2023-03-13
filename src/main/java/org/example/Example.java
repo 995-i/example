5c931e8310f567f3288e7911bd04be7cd7e3a72c
YuYx <409859709@qq.com>
2023-03-13 22:51:34 +0800

    测试推送到GitHub

---
+++ src/main/java/org/example/Example.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Package: org.example
+ * Description: 测试GitHub提交
+ *
+ * @Author YuYx
+ * @Create 2023/3/13 22:35
+ * @Version 1.0
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub");
+    }
+}
